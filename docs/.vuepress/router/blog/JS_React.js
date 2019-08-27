709c77009a65beb058a65b8724adc7fd3250a352
leo_nameless <1150328467@qq.com>
2019-08-27 15:23:59 +0800

    [#fixed]

--- docs/.vuepress/router/blog/JS_React.js
+++ docs/.vuepress/router/blog/JS_React.js
@@ -1,8 +1,8 @@
 const JS_React = {
     title: 'JS框架-React篇',
     children: [
-        'React相关/为什么会出现React_Hooks？',
-        'React相关/React中的生命周期'
+        'React相关/为什么会出现React_Hooks？'
+        // 'React相关/React中的生命周期'
     ]
 }
 
